f7a14eb0732d789f77e81dfe97bdfc8b819e6c60
Manas <manasgedam26@gmail.com>
2023-04-02 20:28:34 +0530

    web live

--- ChatBot/settings.py
+++ ChatBot/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m#b1@%y*-=%q8dhrj884bnaz#s952xtzc$@3+%ik_r2()lfkti'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.github.io']
 
