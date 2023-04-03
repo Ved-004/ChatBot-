b68e1be0c4a8f8f02c8a870c44937aacd281277b
Manas <manasgedam26@gmail.com>
2023-04-03 22:56:56 +0530

    vercel live take-8

--- ChatBot/urls.py
+++ ChatBot/urls.py
@@ -13,10 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 from ChatBot import views
-from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -35,8 +36,8 @@ urlpatterns = [
     path('deleteElement/<str:subKeyVal>/<str:inputField>/',views.deleteElement,name='deleteElement'),
     path('updatedKey/<str:subKeyVal>/<str:inputField>/<str:updatedKey>/', views.keyupdate, name='updatedKey'),
     path('updatedAns/<str:subKeyVal>/<str:inputField>/<str:updatedAnswer>/', views.ansupdate, name='updatedAns')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 # serve static files during development
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
